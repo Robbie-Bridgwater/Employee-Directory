78119cd9c8f8823e9d8c6e69a212c649412e4e26
Robbi <Robbie_Bridgwater@Hotmail.co.uk>
2021-05-09 11:28:38 +0100

    added wrapper components that contains the page, container component that will contain everything other than the header, and a searchBody page which contain the main search contents of the employee directory

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import Wrapper from "./components/wrapper";
+import searchBody from "./pages/searchBody";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div>
-          <Route exact path="/" />
+          <Wrapper>
+          <Route exact path="/" component={searchBody} />
+          </Wrapper>
       </div>
     </Router>
   );
