9afd9143b62d1cd44e34acdcdeaf0701134d9fdf
Robbie Bridgwater <71599797+Robbie-Bridgwater@users.noreply.github.com>
2021-05-09 18:23:16 +0100

    Update index.js

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -8,7 +8,7 @@ function Search() {
     <div>
       <h1 className="title">EMPLOYEE DIRECTORY</h1>
       <Container style={{ minHeight: "100vh" }}>
-        <p class="info">Below you can: (i) order the results alphabetically by name; (ii) or search the name (or email) of the user you wish to find and the results will display live for you as you type.</p>
+        <p class="info">Below you can: (i) order the results alphabetically by name; (ii) or search the name (or email) of the user you wish to find and the results will display automatically as you type.</p>
         <SearchResults />
       </Container>
     </div>
