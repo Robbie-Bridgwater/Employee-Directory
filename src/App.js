5ab1399f2bbef7781b7003393c559d56509e24f4
Robbi <Robbie_Bridgwater@Hotmail.co.uk>
2021-05-09 10:39:15 +0100

    the basic framework to the app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div>
+          <Route exact path="/" />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
